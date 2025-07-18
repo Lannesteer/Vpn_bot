a6516dc2388d677b0a3f4ac7c486948571152951
Lannesteer <degterev907@gmail.com>
2025-07-18 23:23:17 +0300

    add scoped_session() for celery tasks

---
+++ src/celery_worker/session.py
@@ -0,0 +1,25 @@
+import asyncio
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from celery import current_task
+from sqlalchemy.ext.asyncio import async_scoped_session, AsyncSession
+
+from database.session import AsyncDatabase
+
+loop = asyncio.get_event_loop()
+
+session_factory = AsyncDatabase.get_session_maker()
+
+
+@asynccontextmanager
+async def scoped_session() -> AsyncGenerator[AsyncSession, None]:
+    scoped_factory = async_scoped_session(
+        session_factory,
+        scopefunc=current_task,
+    )
+    try:
+        async with scoped_factory() as session:
+            yield session
+    finally:
+        await scoped_factory.remove()
--- src/celery_worker/tasks.py
+++ src/celery_worker/tasks.py
@@ -1,5 +1,4 @@
-import asyncio
-
+from celery_worker.session import loop, scoped_session
 from src.database.keys.service import key_service
 from src.database.users.service import user_service
 from src.handlers.users.vpn.utils import vpn_utils
@@ -9,23 +8,22 @@ from src.celery_worker.celery_worker import celery_app
 
 @celery_app.task
 def check_balance(telegram_id, key_id):
-    """
-    Проверяет баланс пользователя и удаляет ключ через 30 мин, если недостаточно средств.
-    """
+    return loop.run_until_complete(check_balance_async(telegram_id, key_id))
 
-    async def async_task():
-        user = await user_service.get_user_by_telegram_id(telegram_id)
-        key = await key_service.get_key(key_id)
+
+async def check_balance_async(telegram_id, key_id):
+    async with scoped_session() as session:
+        user = await user_service.get_user_by_telegram_id(session, telegram_id)
+        key = await key_service.get_key(session, key_id)
+
         if user.balance < 200:
-            await key_service.delete(key.id)
+            await key_service.delete(session, key.id)
             await vpn_utils.notify_users(
                 telegram_id=telegram_id,
                 text=f'Не удалось списать стоимость ключа с сервера '
                      f'{key.server.country}, 100GB, {key.server.price}₽/месяц  '
                      f'сумме {key.server.price}. Ключ удален',
-            ),
+            )
         else:
             data = UserUpdate(balance=user.balance - 200)
-            await user_service.update(data, user.id)
-
-    asyncio.run(async_task())
+            await user_service.update(session, data, user.id)
