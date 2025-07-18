cf103a7c1d91e63ad8f7e065f34ba5f0b4e675fe
Lannesteer <degterev907@gmail.com>
2025-07-19 00:18:03 +0300

    fix

--- src/celery_worker/tasks.py
+++ src/celery_worker/tasks.py
@@ -32,11 +32,11 @@ def check_balance(telegram_id, key_id):
 
 async def check_balance_async(telegram_id, key_id):
     async with scoped_session() as session:
-        user = await user_service.get_user_by_telegram_id(session, telegram_id)
-        key = await key_service.get_key(session, key_id)
+        user = await user_service.get_user_by_telegram_id(telegram_id)
+        key = await key_service.get_key(key_id)
 
         if user.balance < 200:
-            await key_service.delete(session, key.id)
+            await key_service.delete(key.id)
             await vpn_utils.notify_users(
                 telegram_id=telegram_id,
                 text=f'Не удалось списать стоимость ключа с сервера '
@@ -45,4 +45,4 @@ async def check_balance_async(telegram_id, key_id):
             )
         else:
             data = UserUpdate(balance=user.balance - 200)
-            await user_service.update(session, data, user.id)
+            await user_service.update(data, user.id)
